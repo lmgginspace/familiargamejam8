38c4f84bda39b277fefd8cacf92f03e7ab577447
Luis <lmgginspace@gmail.com>
2017-03-24 23:50:08 +0100

    Barras HP y MP

---
+++ Assets/CanvasBar.cs
@@ -0,0 +1,22 @@
+﻿using Extensions.System;
+using System;
+using UnityEngine;
+
+public class CanvasBar : MonoBehaviour
+{
+	// ---- ---- ---- ---- ---- ---- ---- ----
+	// Fields
+	// ---- ---- ---- ---- ---- ---- ---- ----
+	[SerializeField]
+	private RectTransform barFill;
+
+	// ---- ---- ---- ---- ---- ---- ---- ----
+	// Properties
+	// ---- ---- ---- ---- ---- ---- ---- ----
+	public float FillPercentage
+	{
+		get { return this.barFill.anchorMax.x; }
+		set { this.barFill.anchorMax = new Vector2(0.0f, value.ClampToUnit()); }
+	}
+
+}

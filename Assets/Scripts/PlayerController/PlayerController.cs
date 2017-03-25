0520d88b562fc2a94ef136291cb39dbf57384815
KanedaXIII <mnemosineii@gmail.com>
2017-03-25 17:08:10 +0100

    Player Controller

---
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public List<Card> cardsList;
+
+    public
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+         
+        
+
+        switch ()
+        {
+                Input.GetKeyDown();
+        }
+
+
+       if(Input.GetMouseButtonDown(0) )
+        {
+            
+        }
+
+	}
+
+    public void spawnP()
+    {
+
+    }
+
+}

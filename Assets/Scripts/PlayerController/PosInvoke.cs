12484a8f9ab97838703f2d77c69906f62f4323f2
KanedaXIII <mnemosineii@gmail.com>
2017-03-25 18:49:31 +0100

    Minion

---
+++ Assets/Scripts/PlayerController/PosInvoke.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosInvoke : MonoBehaviour {
+
+    public string posName;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

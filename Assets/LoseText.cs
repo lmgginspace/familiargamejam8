ff5b010129eb3a3bd4dfaa64b145d657e48831b9
dandelea <dandelea@alum.us.es>
2017-03-26 14:00:33 +0200

    a

---
+++ Assets/LoseText.cs
@@ -0,0 +1,20 @@
+﻿/*
+* author: Daniel
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseText : MonoBehaviour {
+
+    public AudioClip crySound;
+
+	#region Unity Functions
+
+	void Start () {
+        AudioManager.Instance.PlaySoundEffect(crySound);
+	}
+
+	#endregion
+}
--- Assets/WinText.cs
+++ Assets/WinText.cs
@@ -12,6 +12,7 @@ public class WinText : MonoBehaviour {
 
     #region Variables
     private string[] phrases;
+    public AudioClip winSound;
 	#endregion
 
 	#region Unity Functions
@@ -19,6 +20,7 @@ public class WinText : MonoBehaviour {
 	void Start () {
         phrases = new string[] { "I told you those eggs would work", "The end is near", "I swear master did wink at me once"};
         GetComponent<Text>().text = RandomUtil.NextInRange(phrases);
+        AudioManager.Instance.PlaySoundEffect(winSound);
 	}
 
 	#endregion

b30d63fc0db78d30961f84c128d3158fe7845070
Luis <lmgginspace@gmail.com>
2017-03-26 15:44:28 +0200

    Cards

---
+++ Assets/CanvasCard.cs
@@ -0,0 +1,27 @@
+﻿using Extensions.System;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasCard : MonoBehaviour
+{
+	[SerializeField]
+	private Image bottleFill;
+	[SerializeField]
+	private Image cardImage;
+
+	public float BottleFill
+	{
+		get { return this.bottleFill.fillAmount; }
+		set { this.bottleFill.fillAmount = value.ClampToUnit(); }
+	}
+
+	public bool Active
+	{
+		set
+		{
+			this.cardImage.color = value ? Color.white : Color.gray;
+		}
+	}
+
+}
